cda26469123ccfca62e721d1f1bf865180f134b8
Hemant Kumar Soni <hemant.soni@daimler.com>
2021-03-01 09:41:43 +0100

    search result componenet added

---
+++ src/app/partshome/components/part/searchresult/searchresult.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'searchresult',
+  templateUrl: './searchresult.component.html',
+  styleUrls: ['./searchresult.component.css']
+})
+export class SearchresultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

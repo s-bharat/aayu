f8c6282cfe98d5671afc3747aceef876478609a6
s-bharat <76701531+s-bharat@users.noreply.github.com>
2021-03-03 18:52:26 +0530

    user obj

--- src/app/partshome/service/part/partObj.ts
+++ src/app/partshome/service/part/partObj.ts
@@ -1,7 +1,10 @@
+import { userObj } from "./userObj";
+
 export interface PartObj {
     partNumber :string;
     fullPartName: string;
     version: string;
     partStatus: string;
     lastUpdatedDate:String;
+    user:userObj;
     }

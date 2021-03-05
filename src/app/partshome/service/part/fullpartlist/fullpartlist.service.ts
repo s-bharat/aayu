4d2c60710d3f0296538bc4ba18e7f7d47ecda734
Hemant Kumar Soni <hemant.soni@daimler.com>
2021-03-05 18:33:20 +0100

    Cleaned code again.

--- src/app/partshome/service/part/fullpartlist/fullpartlist.service.ts
+++ src/app/partshome/service/part/fullpartlist/fullpartlist.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { PartnumberfieldsObj } from '../partnumberfieldsObj';
+import { PartnumberObj } from './partnumberObj';
 
 
 @Injectable({

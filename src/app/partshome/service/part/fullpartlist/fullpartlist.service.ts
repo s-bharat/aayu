5923221aca5d02a9508342c1df6be2b788ec4811
ayushi patwa <ayushipatwa21@gmail.com>
2021-03-05 18:20:28 +0530

    change

--- src/app/partshome/service/part/fullpartlist/fullpartlist.service.ts
+++ src/app/partshome/service/part/fullpartlist/fullpartlist.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { PartnumberfieldsObj } from '../partnumberfieldsObj';
+import { PartnumberdataObj } from '../partnumberfieldsObj';
 
 
 @Injectable({
@@ -13,8 +13,8 @@ export class FullpartlistService {
 
   constructor(private http:HttpClient) { }
 
-  getPartsdata(): Observable<PartnumberObj[]>{
-    return this.http.get<PartnumberObj[]>(this.partList)
+  getPartsdata(): Observable<PartnumberdataObj []>{
+    return this.http.get<PartnumberdataObj[]>(this.partList)
 
   }
 }
